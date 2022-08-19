a1b138e06bd176c627f8fec2d40871cba5e6914d
Benoit <90827157+Yurhigz@users.noreply.github.com>
2022-08-19 13:31:50 +0200

    Delete scripti.sql

--- scripti.sql
+++
@@ -1,5 +0,0 @@
-LOAD DATA LOCAL INFILE 'C:/Users/benoi/Desktop/Programmation/PROJECTS/SQL_TELECOM_CHURN/telecom_customer_churn.csv' INTO TABLE telecom_customer_churn
-FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-select * from telecom_customer_churn;
